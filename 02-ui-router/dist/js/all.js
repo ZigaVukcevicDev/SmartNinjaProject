f5ece1d39efc54837b2b5773776fbee0d1f1fbc7
Miha Susnik <mihec.susnik@gmail.com>
2015-05-29 17:40:49 +0200

    Added additional states.

--- 02-ui-router/dist/js/all.js
+++ 02-ui-router/dist/js/all.js
@@ -1,10 +1,12 @@
 angular.module('app', ['ui.router']);
-angular.module('app').config(function($stateProvider){
+angular.module('app').config(function($stateProvider, $urlRouterProvider){
+
+	$urlRouterProvider.otherwise('/error');
 
 	$stateProvider.state('home',
 	{
-		url: '/home',
-		template: '<h1>To je domača stran</h1>'
+		url: '/',
+		templateUrl: '<h1>To je domača stran</h1>'
 	});
 
 	$stateProvider.state('login',
@@ -13,7 +15,45 @@ angular.module('app').config(function($stateProvider){
 		template: '<h1>Prijavi se</h1>'
 	});
 
+	$stateProvider.state('error',
+	{
+		url: '/error',
+		template: '<h2>Prišlo je do napake.</h2>'
+	});
+
+	$stateProvider.state('parent',
+	{
+		url: '/parent',
+		template: '<h1>Jaz sem parent</h1><ui-view></ui-view>'
+	});
+
+	$stateProvider.state('parent.child', 
+	{
+		url: '/child',
+		template: '<h4>Jaz sem child.</h4>'
+	})
+
+	$stateProvider.state('parameter', {
+		url: '/parameter/:name',
+		template: '<h1>Parameter</h1>',
+		controller: function($stateParams, $state){
+			//alert($stateParams.name);
+
+			//$state.go('parent');
+
+			//$state.reload();
+
+			/*if($state.is('parent.child'))
+			{
+
+			}*/
+
+		}
+	});
+
 });
+
+
 angular.module('app').controller('ExampleController', function($scope){
 
 	$scope.example = 'Example from ExampleController';
